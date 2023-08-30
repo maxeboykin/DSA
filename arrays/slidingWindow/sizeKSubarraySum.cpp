2137a94ede8fdd8e7c15561ab86c81af5791ecd7
max boykin <maxboykin@maxs-MBP.lan>
2023-08-30 14:37:27 -0400

    sizeksubarraysum

--- arrays/slidingWindow/sizeKSubarraySum.cpp
+++ arrays/slidingWindow/sizeKSubarraySum.cpp
@@ -1,50 +1,64 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <math.h>
+#include <algorithm>
 /*
- * Given an array of positive numbers and a positive number 'k,'
- * find the maximum sum of any contiguous subarray of size 'k'.
+ * Given an array of positive integers and a number ‘S,’ find the length of the
+ * smallest contiguous subarray whose sum is greater than or equal to 'S'.
+ * Return 0 if no such subarray exists.
  */
 
 using namespace std;
-class MaxSumSubarrayOfSizeK {
+class SmallestSubarrayWithGreaterSum {
 public:
-    static int findMaxSumSubArray(int k, const vector<int> &arr) {
+    static int findMinSubArray(int S, const vector<int> &arr) {
+        int minWindowSize = -1;
+        int curWindowSum = arr[0];
         int left = 0;
         int right = 0;
-        int curSum = 0;
-        int curMaxSum = - INFINITY; // somehow doesnt get changed in the second test. it keeps max of 9 from test one....
-        while(right < arr.size() && right < k){  //guarding if k is greater than arr size
-            curSum += arr[right];
-            right++;
+
+        // initialize first window
+        while(right < arr.size() && curWindowSum < S){
+            right ++;
+            curWindowSum += arr[right];
         }
-        if(curSum > curMaxSum) curMaxSum = curSum;
-        left++;
+        // set first minWindowSize
+        minWindowSize = right - left + 1;
         while(right < arr.size()){
-            curSum -= arr[left-1];
-            curSum += arr[right];
-            if(curSum > curMaxSum) curMaxSum = curSum;
-            left++;
-            right++;
+            while(left <= right && curWindowSum >= S){
+                if(left == right) return 1;
+                minWindowSize = min(minWindowSize, right - left + 1);
+                left++;
+                curWindowSum -= arr[left - 1];
+            }
+            while(right < arr.size() && curWindowSum < S){
+                right++;
+                curWindowSum += arr[right];
+            }
         }
-        return curMaxSum;
+        return minWindowSize;
     }
 };
 
 int main(){
-    MaxSumSubarrayOfSizeK solution;
-    vector<int> foo = {2, 1, 5, 1, 3, 2};
-    int k1 = 3;
-    int expectedResult = 9;
-    vector<int> bar = {2, 3, 4, 1, 5};
-    int k2 = 2;
-    int expectedResultTwo = 7;
-    int resultOne = solution.findMaxSumSubArray(k1, foo);
-    int resultTwo = solution.findMaxSumSubArray(k2, bar);
+    SmallestSubarrayWithGreaterSum solution;
+    vector<int> foo = {2, 1, 5, 2, 3, 2};
+    int s1 = 7;
+    int expectedResult = 2;
+    vector<int> bar = {2, 1, 5, 2, 8};
+    int s2 = 7;
+    int expectedResultTwo = 1;
+    vector<int> zing = {3, 4, 1, 1, 6};
+    int s3 = 8;
+    int expectedResultThree = 3;
+    int resultOne = solution.findMinSubArray(s1, foo);
+    int resultTwo = solution.findMinSubArray(s2, bar);
+    int resultThree = solution.findMinSubArray(s3, zing);
     cout << "findMaxSumSubArray: ";
     cout << "Expected result: " << expectedResult << " actual: " << resultOne;
     cout << endl;
     cout << "Expected result: " << expectedResultTwo << " actual: " << resultTwo;
+    cout << endl;
+    cout << "Expected result: " << expectedResultThree << " actual: " << resultThree;
     cout << endl;
 }
