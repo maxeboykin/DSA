cd9db1dddfc5407dad1cbfeafa2a31808a525d07
max boykin <maxboykin@maxs-MBP.lan>
2023-09-11 11:20:21 -0400

    subset with duplicates

--- recursion/subsets/subsetsWithDuplicates.cpp
+++ recursion/subsets/subsetsWithDuplicates.cpp
@@ -1,24 +1,35 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <string>
+#include <ctime>
+#include <cctype>
 
-class Subsets {
+class SubsetsWithDuplicates {
 public:
     std::vector<std::vector<int>> findSubsets(std::vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
         std::vector<std::vector<int>> subsets;
         subsets.push_back(std::vector<int>{});
-        for(auto current: nums){
-            int n = subsets.size(); // need this since subsets size will increase in next loop
-            for(int i = 0; i < n; i++){
-                std::vector<int> curSet = subsets[i];
-                curSet.push_back(current);
+        int startIndex = 0;
+        int endIndex = 0;
+        for(int i = 0; i < nums.size(); i++){
+            startIndex = 0;
+            if (i > 0 && nums[i] == nums[i-1]){
+                startIndex = endIndex + 1;
+            }
+            endIndex = subsets.size() -1;
+            for(int j = startIndex; j <= endIndex; j++){
+                std::vector<int> curSet = subsets[j];
+                curSet.push_back(nums[i]);
                 subsets.push_back(curSet);
             }
         }
         return subsets;
     }
     void printExpectations(std::vector<std::vector<int>> expected, std::vector<std::vector<int>> actual){
-        std::cout << "Group Anagrams: ";
+        std::cout << "Subsets w Duplicates: ";
         std::cout << "Expected: " << std::endl;
         printNestedArray(expected);
         std::cout << "Actual: " << std::endl;
@@ -37,9 +48,9 @@ public:
 };
 
 int main(){
-    Subsets solution;
-    std::vector<int> foo = {1, 3};
+    SubsetsWithDuplicates solution;
+    std::vector<int> foo = {1, 3, 3};
     std::vector<std::vector<int>> result1 = solution.findSubsets(foo);
-    std::vector<std::vector<int>> expectation1 = {{}, {1}, {5}, {3}, {1,5}, {1,3}, {5,3}, {1,5,3}};
+    std::vector<std::vector<int>> expectation1 = {{}, {1}, {3}, {1,3}, {3,3}, {1,3,3}};
     solution.printExpectations(expectation1, result1);
 }
